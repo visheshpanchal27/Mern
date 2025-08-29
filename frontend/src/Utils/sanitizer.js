9c98e9e196ac0b597295f2f5f7ee96de02a3f258
visheshpanchal27 <visheshpanchal1212.com>
2025-08-29 14:49:12 +0530

    change in google auth

--- frontend/src/Utils/sanitizer.js
+++ frontend/src/Utils/sanitizer.js
@@ -1,18 +1,9 @@
-import DOMPurify from 'dompurify';
-
-// Sanitize user input to prevent XSS
+// Simple input sanitization without DOMPurify dependency
 export const sanitizeInput = (input) => {
   if (typeof input !== 'string') return input;
-  return DOMPurify.sanitize(input, {
-    ALLOWED_TAGS: [],
-    ALLOWED_ATTR: []
-  });
-};
-
-// Sanitize HTML content
-export const sanitizeHTML = (html) => {
-  return DOMPurify.sanitize(html, {
-    ALLOWED_TAGS: ['b', 'i', 'em', 'strong', 'p', 'br'],
-    ALLOWED_ATTR: []
-  });
+  return input
+    .replace(/[<>]/g, '') // Remove < and >
+    .replace(/javascript:/gi, '') // Remove javascript: protocol
+    .replace(/on\w+=/gi, '') // Remove event handlers
+    .trim();
 };
