504d1b8951a63ea64b6cb257bcf3b19ce89b5a9b
visheshpanchal27 <visheshpanchal1212.com>
2025-09-06 13:41:02 +0530

    fix index.js file

--- backend/middlewares/securityMiddleware.js
+++ backend/middlewares/securityMiddleware.js
@@ -3,33 +3,7 @@ import rateLimit from 'express-rate-limit';
 
 // Enhanced security headers
 export const securityHeaders = helmet({
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
-      fontSrc: ["'self'", "https://fonts.gstatic.com", "data:"],
-      imgSrc: ["'self'", "data:", "https://res.cloudinary.com", "https://www.paypalobjects.com"],
-      scriptSrc: [
-        "'self'", 
-        "'unsafe-inline'", 
-        "'unsafe-eval'", 
-        "https://accounts.google.com", 
-        "https://apis.google.com", 
-        "https://www.googleapis.com",
-        "https://www.paypal.com",
-        "https://js.paypal.com"
-      ],
-      connectSrc: [
-        "'self'", 
-        "https://api.paypal.com", 
-        "https://www.googleapis.com",
-        "https://accounts.google.com",
-        "https://mern-u0bv.onrender.com",
-        "https://mern-j0z9.onrender.com"
-      ],
-      frameSrc: ["'self'", "https://accounts.google.com", "https://www.paypal.com"],
-    },
-  },
+  contentSecurityPolicy: false,
   crossOriginEmbedderPolicy: false,
   crossOriginOpenerPolicy: false,
 });
