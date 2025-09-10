40dc1335ea4ef19affc00889eb504b34d32c4ebe
visheshpanchal27 <visheshpanchal1212.com>
2025-09-10 17:00:07 +0530

    new update

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: (import.meta.env.VITE_API_URL || 'https://mernbackend-tmp5.onrender.com') + '/api',
+    baseUrl: import.meta.env.VITE_API_URL || 'https://mernbackend-tmp5.onrender.com/api',
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('webToken');
       if (token) {
